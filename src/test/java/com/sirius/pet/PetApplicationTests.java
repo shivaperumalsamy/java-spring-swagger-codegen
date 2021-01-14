eac18fe7222649707d23d32309bcbf37fb5fc9f6
Sivanandham Perumalsamy <sivagasc@gmail.com>
2021-01-14 16:28:42 -0600

    Pet API code generation

---
+++ src/test/java/com/sirius/pet/PetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sirius.pet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ target/generated-sources/src/main/java/com/sirius/petservice/models/InternalError.java
@@ -0,0 +1,51 @@
+package com.sirius.petservice.models;
+
+import java.util.Objects;
+import com.sirius.petservice.models.ErrorResponse;
+import org.springframework.validation.annotation.Validated;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+
+/**
+ * InternalError
+ */
+@Validated
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2021-01-14T15:50:31.375636-06:00[America/Chicago]")
+public class InternalError extends ErrorResponse  {
+
+  @Override
+  public boolean equals(java.lang.Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    return true;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(super.hashCode());
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class InternalError {\n");
+    sb.append("    ").append(toIndentedString(super.toString())).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(java.lang.Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
